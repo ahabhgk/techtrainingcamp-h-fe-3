eb7312d2b46a55f96f2988c21ee3f5e62a631b0d
ahabhgk <ahabhgk@gmail.com>
2020-07-01 21:00:23 +0800

    chore(config): add eslint

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,17 @@
-
-const path = require("path");
+const path = require('path');
 
 function resolve(dir) {
   return path.join(__dirname, dir);
 }
 
 module.exports = {
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     config.resolve.alias
-      .set("@", resolve("./src"))
-      .set("components", resolve("./src/components"))
-      .set("assets", resolve("./src/assets"))
-      .set("common", resolve("./src/common"))
-      .set("network", resolve("./src/network"))
-      .set("views", resolve("./src/views"));
-  }
+      .set('@', resolve('./src'))
+      .set('components', resolve('./src/components'))
+      .set('assets', resolve('./src/assets'))
+      .set('common', resolve('./src/common'))
+      .set('network', resolve('./src/network'))
+      .set('views', resolve('./src/views'));
+  },
 };
