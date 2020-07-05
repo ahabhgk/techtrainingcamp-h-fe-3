d428e2aeecf2c64dd8c8a411c0afbc5cf524633d
ahabhgk <ahabhgk@gmail.com>
2020-07-05 10:43:55 +0800

    refactor(network): request 优化，代码格式化
    1. request 方法的 instance 实例放在了 request 方法外，防止每次请求都会重新创建一遍实例

--- src/components/god/PlayerStatus.vue
+++ src/components/god/PlayerStatus.vue
@@ -1,23 +1,22 @@
-
 <template>
   <div class="playerstatus">
-    
+
   </div>
 </template>
 
 <script>
-  export default {
-    name: "PlayerStatus",
-    components: {},
-    data() {
-      return {};
-    },
-    computed: {},
-    watch: {},
-    methods: {},
-    created() {},
-    mounted() {},
-  };
+export default {
+  name: 'PlayerStatus',
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  created() {},
+  mounted() {},
+};
 </script>
 <style scoped>
 </style>
