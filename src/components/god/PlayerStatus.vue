d985e312d2c9b84c7db167047dd58cdad32cfb6e
ahabhgk <ahabhgk@gmail.com>
2020-07-05 21:45:28 +0800

    feat(view, components): 大致完成 god/game 界面样式

--- src/components/god/PlayerStatus.vue
+++ src/components/god/PlayerStatus.vue
@@ -1,22 +1,187 @@
 <template>
-  <div class="playerstatus">
-
+  <div class="list-item">
+    <div class="player">
+      <img class="avatar" :src="player[role].avatar" />
+      <div>
+        <div class="role">{{ player[role].role }}</div>
+        <div class="name">{{ name }}</div>
+      </div>
+    </div>
+    <div class="control-wrapper">
+      <button
+        class="control"
+        v-for="status of statusList"
+        :key="status"
+        @click="handleSetPlayerStatus(status)"
+      >
+        <img class="icon" :src="rebornStatusSet.has(status) ? icon.potions : icon.skull"
+        />
+        <span>{{ statusMap[status] }}</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+// import {} from '@vue/composition-api';
+import useRouter from 'utils/use-router';
+import { roleType } from 'constants';
+import iconSkull from 'assets/images/icon/skull.png';
+import iconPotions from 'assets/images/icon/potions.png';
+import avatarAcient from 'assets/images/player/acient.jpg';
+import avatarHunter from 'assets/images/player/hunter.jpg';
+import avatarIdiot from 'assets/images/player/idiot.jpg';
+import avatarSavior from 'assets/images/player/savior.jpg';
+import avatarSeer from 'assets/images/player/seer.jpg';
+import avatarSheriff from 'assets/images/player/sheriff.jpg';
+import avatarVillager from 'assets/images/player/villager.jpg';
+import avatarWerewolf from 'assets/images/player/werewolf.jpg';
+import avatarWitch from 'assets/images/player/witch.jpg';
+
+const icon = {
+  skull: iconSkull,
+  potions: iconPotions,
+};
+
+const player = {
+  [roleType.ACIENT]: {
+    avatar: avatarAcient,
+    role: '祖先',
+  },
+  [roleType.HUNTER]: {
+    avatar: avatarHunter,
+    role: '猎人',
+  },
+  [roleType.IDIOT]: {
+    avatar: avatarIdiot,
+    role: '愚人',
+  },
+  [roleType.SAVIOR]: {
+    avatar: avatarSavior,
+    role: '救助者',
+  },
+  [roleType.SEER]: {
+    avatar: avatarSeer,
+    role: '预言家',
+  },
+  [roleType.SHERIFF]: {
+    avatar: avatarSheriff,
+    role: '警长',
+  },
+  [roleType.VILLAGER]: {
+    avatar: avatarVillager,
+    role: '村民',
+  },
+  [roleType.WEREWOLF]: {
+    avatar: avatarWerewolf,
+    role: '狼人',
+  },
+  [roleType.WITCH]: {
+    avatar: avatarWitch,
+    role: '女巫',
+  },
+};
+
+const statusMap = {
+  cured: '治愈',
+  sheriff: '警长',
+  poisoned: '毒药',
+  protected: '保护',
+  killed: '被杀',
+  shot: '猎枪',
+  voted: '踢出',
+};
+
+const rebornStatusSet = new Set([
+  'cured',
+  'protected',
+]);
+
 export default {
   name: 'PlayerStatus',
-  components: {},
-  data() {
-    return {};
+  props: {
+    name: String,
+    role: String,
+    statusList: Array,
   },
-  computed: {},
-  watch: {},
-  methods: {},
-  created() {},
-  mounted() {},
+  setup(props) {
+    const { currentRoute } = useRouter();
+    console.log(currentRoute);
+    const handleSetPlayerStatus = (status) => {
+      console.log(`request: /god/setStatus?name=${props.name}&status=${status}&room=${currentRoute.query.room}&token=${currentRoute.query.token}`);
+    };
+
+    return {
+      handleSetPlayerStatus,
+      rebornStatusSet,
+      statusMap,
+      icon,
+      player,
+    };
+  },
 };
 </script>
-<style scoped>
+
+<style scoped lang="scss">
+@import "../../assets/styles/utils.scss";
+
+.list-item {
+  margin: 4px;
+  height: 60px;
+  border-radius: 4px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #494a68;
+  &:first-child {
+    border-top: none;
+  }
+  .player {
+    margin-left: 10px;
+    width: 170px;
+    display: flex;
+    align-items: center;
+    .avatar {
+      margin-right: 10px;
+      width: 40px;
+      height: 40px;
+      border: #000 solid 2px;
+    }
+    .role {
+      font-size: 18px;
+      font-weight: bold;
+      color: #fdfdfd;
+    }
+    .name {
+      font-size: 16px;
+      font-weight: bold;
+      color: #fdfdfd;
+    }
+  }
+}
+
+.control-wrapper {
+  display: flex;
+  .control {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 40px;
+    width: 40px;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    @include btn-ripple;
+  }
+  .control > span {
+    color: #ffffff;
+    font-size: 12px;
+  }
+  .icon {
+    width: 20px;
+    height: 20px;
+  }
+}
+
 </style>
--- src/views/god/Game.vue
+++ src/views/god/Game.vue
@@ -1,22 +1,99 @@
 <template>
-  <div class="game">
-
+  <div class="wrapper">
+    <div class="title">上帝操控页</div>
+    <div class="list-wrapper">
+      <PlayerStatus
+        v-for="player in state.players"
+        :key="player.name"
+        :name="player.name"
+        :role="player.role"
+        :statusList="player.status"
+      />
+    </div>
+    <div class="end-btn-wrapper">
+      <button class="end-btn">
+        <img :src="icon.end" />
+        <span>强行结束</span>
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
+import { reactive } from '@vue/composition-api';
+import PlayerStatus from 'components/god/PlayerStatus.vue';
+import { roleType } from 'constants';
+import iconEnd from 'assets/images/icon/end.png';
+
 export default {
   name: 'Game',
-  components: {},
-  data() {
-    return {};
+  components: {
+    PlayerStatus,
   },
-  computed: {},
-  watch: {},
-  methods: {},
-  created() {},
-  mounted() {},
+  setup() {
+    const state = reactive({
+      players: [
+        { name: 'bkk', role: roleType.SEER, status: ['voted', 'killed', 'cured', 'poisoned'] },
+        { name: 'yiMing', role: roleType.WITCH, status: ['voted', 'killed', 'cured', 'poisoned'] },
+        { name: 'xxx', role: roleType.HUNTER, status: ['voted', 'killed', 'cured', 'poisoned'] },
+        { name: 'cww', role: roleType.WEREWOLF, status: ['voted', 'cured', 'poisoned'] },
+        { name: 'nddd', role: roleType.WEREWOLF, status: ['voted', 'cured', 'poisoned'] },
+        { name: 'bzzb', role: roleType.VILLAGER, status: ['voted', 'killed', 'cured', 'poisoned'] },
+        { name: 'jackMa', role: roleType.VILLAGER, status: ['voted', 'killed', 'cured', 'poisoned'] },
+        { name: 'teacherMa', role: roleType.VILLAGER, status: ['voted', 'killed', 'cured', 'poisoned'] },
+      ],
+    });
+
+    return {
+      state,
+      icon: { end: iconEnd },
+    };
+  },
 };
 </script>
-<style scoped>
+
+<style scoped lang="scss">
+@import "../../assets/styles/utils.scss";
+
+.wrapper {
+  position: relative;
+  min-height: 100vh;
+  background-color: #424361;
+  padding: 10px 10px 90px;
+  box-sizing: border-box;
+  .title {
+    font-size: 22px;
+    font-weight: bold;
+    color: #74d358;
+    margin: 4px;
+    margin-bottom: 16px;
+  }
+  .end-btn-wrapper {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 20px;
+    width: 80px;
+    margin: 20px auto 0;
+    .end-btn {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      width: 80px;
+      height: 60px;
+      background-color: transparent;
+      border: none;
+      outline: none;
+      @include btn-ripple;
+      & > span {
+        color: #fdfdfd;
+        font-size: 14px;
+      }
+      & > img {
+        width: 40px;
+        height: 40px;
+      }
+    }
+  }
+}
 </style>
