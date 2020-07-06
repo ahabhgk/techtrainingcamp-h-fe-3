606f1f3423ae47e3f064ce8690465c8cf3175448
白开帅 <2430158129@qq.com>
2020-07-06 15:54:13 +0800

    feat: homePage
    首页的基本样式与基本路由
    
    BREAKING CHANGE: 加入'linebreak-style': 'off'防止win无法运行
    
    view-homePage

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-shadow': 'off',
+    'linebreak-style': 'off',
   },
 };
