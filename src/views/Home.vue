7b9c56c049fac4f046eb6c396d6fc77b06f816c3
白开帅 <2430158129@qq.com>
2020-07-06 17:28:18 +0800

    feat: homePage style fixed
    修改了首页的样式和优化了一点代码

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,25 +2,24 @@
   <div class="home">
     <div class='title'><img :src="img.title" /></div>
     <div class='logo'><img :src="img.logo" /></div>
-    <div>{{ state.value }}</div>
-    <div class='home-button' v-for="(item,i) in state.tags" :key='i'>
-      <img :class="`${item.class}Icon`" :src="item.icon"/>
-      <button :class="`${item.class}`" @click="joinGo(`${item.url}`)">{{item.name}}</button>
+    <div class='home-button' v-for="(item,i) in tags" :key='i'>
+      <div><img :class="`${item.class}Icon`" :src="item.icon"/></div>
+      <button :class="`${item.class}`" @click="pushPath(`${item.url}`)">{{item.name}}</button>
     </div>
   </div>
 </template>
 
 <script>
-import { reactive, watchEffect } from '@vue/composition-api';
 import Logo from 'assets/images/logo/logo.png';
 import Title from 'assets/images/HomePage/title.png';
 import Join from 'assets/images/HomePage/join.png';
 import Room from 'assets/images/HomePage/room.png';
 import Score from 'assets/images/HomePage/score.png';
+import useRouter from '../utils/use-router';
 
 const tags = [
   {
-    icon: Join, name: '加入游戏', class: 'join', url: '/god/game',
+    icon: Join, name: '加入游戏', class: 'join', url: '/player/login',
   }, // 暂时
   {
     icon: Room, name: '创建房间', class: 'room', url: '/god/admin',
@@ -33,26 +32,15 @@ export default {
   name: 'Home',
   components: {},
   setup() {
-    const self = this;
-    const state = reactive({
-      value: 0,
-      tags,
-      joinGo(url) {
-        self.$router.push({ path: url });
-      },
-    });
-    watchEffect(() => {
-      console.log(state.value);
-    });
+    const router = useRouter();
+    const pushPath = (url) => {
+      router.push({ path: url });
+    };
     return {
-      state,
+      tags,
+      pushPath,
       img: { logo: Logo, title: Title },
     };
-  },
-  methods: {
-    joinGo(url) {
-      this.$router.push({ path: url });
-    },
   },
 };
 </script>
@@ -75,35 +63,40 @@ export default {
   .home-button{
     text-align: center;
     margin: 30px auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     img{
       width: 20px;
       height: 20px;
       line-height: 20px;
     }
     button{
+      justify-items: center;
       width: 100px;
       border: none;
       outline: none;
-  padding: 12px 12px;
-    color: rgb(56, 48, 48);
-  border: 0 none;
-  border-radius: 36px;
-    font-size: 15px;
-  font-weight: 500;
-  line-height: 1.3;
-    justify-content: center;
-  align-items: center;
- @include btn-ripple;
+      margin-left: 10px;
+      padding: 12px 12px;
+      color: rgb(56, 48, 48);
+      border: 0 none;
+      border-radius: 36px;
+      font-size: 15px;
+      font-weight: 500;
+      line-height: 1.3;
+      justify-content: center;
+      align-items: center;
+      @include btn-ripple;
+    }
+    .join{
+      background:#3dd28d;
+    }
+    .room{
+      background: #ea554e;
+    }
+    .score{
+      background: #feee7d;
+    }
   }
-      .join{
-    background:#3dd28d;
-        }
-        .room{
-          background: #ea554e;
-        }
-        .score{
-          background: #feee7d;
-        }
-}
 }
 </style>
