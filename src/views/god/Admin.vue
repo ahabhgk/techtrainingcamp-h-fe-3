f349448aa9a93277ed469147042af4f188c46285
haoosz <haoshaozhe@outlook.com>
2020-07-14 12:11:04 +0800

    feat: uI modified

--- src/views/god/Admin.vue
+++ src/views/god/Admin.vue
@@ -62,91 +62,91 @@ export default {
 .admin {
   position: relative;
   min-height: 100vh;
-  background-image: linear-gradient(#192033, #0f1225);
+  background-color: #1e272e;
   box-sizing: border-box;
-}
-.title {
-  padding-top: 15vh;
-  padding-bottom: 5vh;
-  text-align: center;
-}
-.select {
-  color: white;
-  background-color: #3c40c6;
-  padding: 10px 20px;
-  border-radius: 2px;
-  font-size: 20px;
-  font-weight: 500;
-}
-.slider {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-bottom: 15vh;
-  background-color: transparent;
-}
-#sinput {
-  -webkit-appearance: none;
-  background: linear-gradient(75deg, #3c40c6, #575fcf);
-  border-radius: 4px;
-  width: 40%;
-  height: 5px;
-  outline: none;
-  box-shadow: 0 0 6px rgb(28, 32, 148);
-}
-#sinput::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  width: 15px;
-  height: 15px;
-  background: #f53b57;
-  border-radius: 50%;
-  transition: 0.3s;
-}
-#sinput:active::-webkit-slider-thumb {
-  background: #ef5777;
-  box-shadow: 0 0 0 6px rgba(155, 73, 146, 0.4);
-}
-.number {
-  margin-right: 20px;
-  background-color: #f53b57;
-  width: 30px;
-  line-height: 30px;
-  text-align: center;
-  border-radius: 3px;
-  color: white;
-  position: relative;
-  font-size: 20px;
-}
-.number::after {
-  content: "";
-  display: block;
-  border-top: 5px solid transparent;
-  border-bottom: 5px solid transparent;
-  border-left: 5px solid #f53b57;
-  position: absolute;
-  top: calc(50% - 5px);
-  right: -4px;
-}
-.button {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 40vh;
-  background-color: transparent;
-}
-.createRoom {
-  background-color: #f53b57; /* Green */
-  border: none;
-  color: white;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  border-radius: 3px;
-  font-size: 20px;
-  outline:none;
-}
-.createRoom:hover {
-  background-color: #f76e82;
+  .title {
+    padding-top: 15vh;
+    padding-bottom: 5vh;
+    text-align: center;
+  }
+  .select {
+    color: white;
+    background-color: #3c40c6;
+    padding: 10px 20px;
+    border-radius: 2px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+  .slider {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 15vh;
+    background-color: transparent;
+  }
+  #sinput {
+    -webkit-appearance: none;
+    background: linear-gradient(75deg, #3c40c6, #575fcf);
+    border-radius: 4px;
+    width: 40%;
+    height: 5px;
+    outline: none;
+    box-shadow: 0 0 6px rgb(28, 32, 148);
+  }
+  #sinput::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    width: 15px;
+    height: 15px;
+    background: #f53b57;
+    border-radius: 50%;
+    transition: 0.3s;
+  }
+  #sinput:active::-webkit-slider-thumb {
+    background: #ef5777;
+    box-shadow: 0 0 0 6px rgba(155, 73, 146, 0.4);
+  }
+  .number {
+    margin-right: 20px;
+    background-color: #f53b57;
+    width: 30px;
+    line-height: 30px;
+    text-align: center;
+    border-radius: 3px;
+    color: white;
+    position: relative;
+    font-size: 20px;
+  }
+  .number::after {
+    content: "";
+    display: block;
+    border-top: 5px solid transparent;
+    border-bottom: 5px solid transparent;
+    border-left: 5px solid #f53b57;
+    position: absolute;
+    top: calc(50% - 5px);
+    right: -4px;
+  }
+  .button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 40vh;
+    background-color: transparent;
+  }
+  .createRoom {
+    background-color: #f53b57; /* Green */
+    border: none;
+    color: white;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 3px;
+    font-size: 20px;
+    outline:none;
+  }
+  .createRoom:hover {
+    background-color: #f76e82;
+  }
 }
 </style>
--- src/views/god/Room.vue
+++ src/views/god/Room.vue
@@ -47,108 +47,107 @@ export default {
 @import "../../assets/styles/utils.scss";
 
 .room {
-  margin: 0;
-  padding: 0;
+  position: relative;
   box-sizing: border-box;
   background-color: #1e272e;
   min-height: 100vh;
-}
-.description {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  padding-top: 20vh;
-  height: 10vh;
-  margin-left: -150px;
-  background-color: transparent;
-  color:#e6e6e6;
-  align-content: left;
-  font-size: 20px;
-}
-.table {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 10vh;
-  background-color: transparent;
-}
-table {
-  border-collapse:separate;
-  border-spacing:10px;
-  background-color: transparent;
-}
-table tr td {
-  font-size: 40px;
-  font-weight: 500;
-  color: rgb(0, 195, 255);
-  text-shadow: 0.5px 0.5px 1px red, 0 0 0.5em rgb(53, 53, 241), 0 0 0.1em rgb(212, 38, 175);
-  text-align: center;
-  width: 40px;
-  height: 40px;
-  border-radius: 20%;
-  border: 1.2px solid rgb(202, 255, 177);
-  padding: 2px;
-  background-color: rgba(10, 2, 2, 0.541);
-}
-#td1:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-#td2:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-#td3:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-#td4:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-#td5:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-#td6:hover {
-  background-color: rgba(44, 44, 26, 0.541);
-}
-.button {
-  text-align: center;
-  padding: 50px;
-}
-.enterRoom {
-  // background-color: transparent;
-  // border: solid rgb(0, 255, 0);
-  // border-width: 0 3px 3px 0;
-  // display: inline-block;
-  // padding: 10px;
-  // -webkit-transform: rotate(-45deg);
-  // outline:none;
-  background-color: rgb(18, 224, 18);
-  border: none;
-  color: rgb(0, 0, 0);
-  font-weight: 500;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  border-radius: 3px;
-  font-size: 20px;
-  outline:none;
-}
-.enterRoom:hover {
-  // border: solid rgb(154, 252, 154);
-  // border-width: 0 3px 3px 0;
-  // display: inline-block;
-  // padding: 5px;
-  // -webkit-transform: rotate(-45deg);
-  // outline:none;
-  background-color:rgb(154, 252, 154);
-  border: none;
-  color: rgb(0, 0, 0);
-  font-weight: 500;
-  padding: 10px 20px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  border-radius: 3px;
-  font-size: 20px;
-  outline:none;
+  .description {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    padding-top: 20vh;
+    height: 10vh;
+    margin-left: -150px;
+    background-color: transparent;
+    color:#e6e6e6;
+    align-content: left;
+    font-size: 20px;
+  }
+  .table {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10vh;
+    background-color: transparent;
+  }
+  table {
+    border-collapse:separate;
+    border-spacing:10px;
+    background-color: transparent;
+  }
+  table tr td {
+    font-size: 40px;
+    font-weight: 500;
+    color: rgb(0, 195, 255);
+    text-shadow: 0.5px 0.5px 1px red, 0 0 0.5em rgb(53, 53, 241), 0 0 0.1em rgb(212, 38, 175);
+    text-align: center;
+    width: 40px;
+    height: 40px;
+    border-radius: 20%;
+    border: 1.2px solid rgb(202, 255, 177);
+    padding: 2px;
+    background-color: rgba(10, 2, 2, 0.541);
+  }
+  #td1:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  #td2:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  #td3:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  #td4:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  #td5:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  #td6:hover {
+    background-color: rgba(44, 44, 26, 0.541);
+  }
+  .button {
+    text-align: center;
+    padding: 50px;
+  }
+  .enterRoom {
+    // background-color: transparent;
+    // border: solid rgb(0, 255, 0);
+    // border-width: 0 3px 3px 0;
+    // display: inline-block;
+    // padding: 10px;
+    // -webkit-transform: rotate(-45deg);
+    // outline:none;
+    background-color: rgb(18, 224, 18);
+    border: none;
+    color: rgb(0, 0, 0);
+    font-weight: 500;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 3px;
+    font-size: 20px;
+    outline:none;
+  }
+  .enterRoom:hover {
+    // border: solid rgb(154, 252, 154);
+    // border-width: 0 3px 3px 0;
+    // display: inline-block;
+    // padding: 5px;
+    // -webkit-transform: rotate(-45deg);
+    // outline:none;
+    background-color:rgb(154, 252, 154);
+    border: none;
+    color: rgb(0, 0, 0);
+    font-weight: 500;
+    padding: 10px 20px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    border-radius: 3px;
+    font-size: 20px;
+    outline:none;
+  }
 }
 </style>
