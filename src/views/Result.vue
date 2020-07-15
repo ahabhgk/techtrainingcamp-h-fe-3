c2296554fa1d849c09e3943febdcd165e6138eb7
RockyChennn <wenwuchen1948@163.com>
2020-07-15 22:50:49 +0800

    feat(result): game result done

--- src/views/Result.vue
+++ src/views/Result.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="wrapper">
-    <div class="title">游戏结果</div>
-    <div class="list-wrapper">
-      <div class="list-item" v-for="(item,index) in state.players" v-bind:key="index">
-        <div class="player">
-          <!-- <img class="avatar"
-          :src="item.status === 'alive' ? player[item.role].avatar : iconDead" /> -->
-          <div>
-            <!-- <div class="role">{{ player[item.role].role }}</div> -->
-            <div class="name">{{ item.name }}</div>
-          </div>
-        </div>
-      </div>
+    <div class="title">{{state.data.winner === "werewolf"?"狼人":"平民"}}胜利</div>
+    <div class='result-lists'>
+      <ul>
+        <li>
+          <a v-for="(item,i) in titles" :key="i" class='list-header'>{{item}}</a>
+        </li>
+      </ul>
+      <ul>
+        <li v-for="(item,i) in state.data.players" :key="i">
+          <a>{{item.name}}</a>
+          <a>{{ roleName[item.role]}}</a>
+          <a :style="{'color':item.status === 'alive'?'lightgreen':'grey'}">
+            {{item.status === "alive" ? "存活":"死亡"}}
+            </a>
+          <a>{{item.killedAt}}</a>
+          <a>{{item.killedBy === "__nobody" ? "-":
+            (item.killedBy === "__vote"?"票死":item.killedBy)}}</a>
+          <a>{{item.points}}</a>
+        </li>
+      </ul>
     </div>
     <div class="btns-wrapper" @click="handleButtonClick">
       <div class="btn" data-index="0">积分榜</div>
@@ -25,55 +33,9 @@
 import { reactive, onMounted } from '@vue/composition-api';
 import useRouter from 'utils/use-router';
 import * as playerApi from 'network/player';
-import { roleType } from 'constants';
-import iconDead from 'assets/images/icon/dead.png';
-import avatarHunter from 'assets/images/player/hunter.jpg';
-import avatarSeer from 'assets/images/player/seer.jpg';
-import avatarVillager from 'assets/images/player/villager.jpg';
-import avatarWerewolf from 'assets/images/player/werewolf.jpg';
-import avatarWitch from 'assets/images/player/witch.jpg';
-// import avatarAcient from 'assets/images/player/acient.jpg';
-// import avatarIdiot from 'assets/images/player/idiot.jpg';
-// import avatarSavior from 'assets/images/player/savior.jpg';
-// import avatarSheriff from 'assets/images/player/sheriff.jpg';
+import { roleName } from 'constants';
 
-// const statusMap = {
-//   cured: '灵药',
-//   sheriff: '警长',
-//   poisoned: '毒药',
-//   protected: '保护',
-//   killed: '被刀',
-//   shot: '猎枪',
-//   voted: '踢出',
-// };
-
-const player = {
-  [roleType.HUNTER]: {
-    avatar: avatarHunter,
-    role: '猎人',
-  },
-  [roleType.SEER]: {
-    avatar: avatarSeer,
-    role: '预言家',
-  },
-  [roleType.VILLAGER]: {
-    avatar: avatarVillager,
-    role: '村民',
-  },
-  [roleType.WEREWOLF]: {
-    avatar: avatarWerewolf,
-    role: '狼人',
-  },
-  [roleType.WITCH]: {
-    avatar: avatarWitch,
-    role: '女巫',
-  },
-};
-
-// const rebornStatusSet = new Set([
-//   'cured',
-//   'protected',
-// ]);
+const titles = ['姓名', '角色', '状态', '天数', '死因', '胜场数'];
 
 export default {
   name: 'Result',
@@ -81,72 +43,6 @@ export default {
   setup() {
     const state = reactive({
       data: '',
-      players: [
-        {
-          name: '玩家1',
-          status: 'dead',
-          role: 'witch',
-          points: 6,
-          killedBy: 'witch',
-          killedAt: 1,
-        },
-        {
-          name: '玩家2',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf',
-          killedAt: 1,
-        },
-        {
-          name: '玩家3',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf',
-          killedAt: 1,
-        },
-        {
-          name: '玩家4',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf|vote|hunter|witch',
-          killedAt: 1,
-        },
-        {
-          name: '玩家5',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf|vote|hunter|witch',
-          killedAt: 1,
-        },
-        {
-          name: '玩家6',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf|vote|hunter|witch',
-          killedAt: 1,
-        },
-        {
-          name: '玩家名7',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf|vote|hunter|witch',
-          killedAt: 1,
-        },
-        {
-          name: '玩家名字8',
-          status: 'alive',
-          role: 'seer',
-          points: 6,
-          killedBy: 'werewolf|vote|hunter|witch',
-          killedAt: 1,
-        },
-      ],
     });
     const router = useRouter();
     const { currentRoute } = router;
@@ -161,8 +57,6 @@ export default {
 
     onMounted(() => {
       getData();
-      console.log(iconDead);
-      console.log(player.HUNTER);
     });
 
     const handleButtonClick = (e) => {
@@ -178,6 +72,8 @@ export default {
 
     return {
       state,
+      roleName,
+      titles,
       handleButtonClick,
     };
   },
@@ -202,78 +98,20 @@ export default {
   }
 }
 
-.list-item {
-  margin: 4px;
-  height: 60px;
-  border-radius: 4px;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  background-color: #494a68;
-  &:first-child {
-    border-top: none;
-  }
-  .player {
-    margin-left: 10px;
-    width: 170px;
-    display: flex;
-    align-items: center;
-    .avatar {
-      margin-right: 10px;
-      width: 40px;
-      height: 40px;
-      border: #000 solid 2px;
-    }
-    .role {
-      font-size: 18px;
-      font-weight: bold;
-      color: #fdfdfd;
-    }
-    .name {
-      font-size: 16px;
-      font-weight: bold;
-      color: #fdfdfd;
-    }
-  }
-}
-
-.control-wrapper {
-  display: flex;
-  .control {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-between;
-    height: 40px;
-    width: 40px;
-    background-color: transparent;
-    border: none;
-    outline: none;
-    @include btn-ripple;
-  }
-  .control > span {
-    color: #ffffff;
-    font-size: 12px;
-  }
-  .icon {
-    width: 20px;
-    height: 20px;
-  }
-}
-
 .btns-wrapper {
   display: flex;
   justify-content: space-around;
   align-items: center;
   height: 30px;
-  margin-top: 20px;
-
+  margin-top: 40px;
   .btn {
     width: 100px;
     line-height: 30px;
     text-align: center;
     border-radius: 15px;
     color: white;
+    font-weight: bold;
+    border: 2px solid #e3e3e3;
     &:nth-child(1) {
       background: red;
     }
@@ -283,6 +121,50 @@ export default {
     &:nth-child(3) {
       background: green;
     }
+  }
+}
+
+.result-lists{
+    margin: 5px;
+    margin-top:0;
+    border-radius:25px;
+    .list-header{
+      display: flex;
+      color: #5b697b;
+      background: #081521;
+      height: 6vh;
+      justify-content: space-around;
+      align-items: center;
+    }
+  ul{
+    counter-reset: section;
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  li {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    line-height: 46px;
+    height: 46px;
+    overflow: hidden;
+    font-size: 14px;
+    text-align:center;
+    a{
+      color: #e1e2e3;
+      width: 25%;
+      max-width: 25%;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+  }
+  li:nth-of-type(odd){
+    background: #112335;
+  }
+  li:nth-of-type(even){
+    background: #091b2c;
   }
 }
 </style>
