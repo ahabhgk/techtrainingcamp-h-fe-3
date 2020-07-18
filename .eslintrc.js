46a0fb56a86b5d92a81866ef5055d01d87eae78f
ahabhgk <ahabhgk@gmail.com>
2020-07-18 22:19:58 +0800

    fix(chore): eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,5 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-shadow': 'off',
-    'linebreak-style': 'off',
-    'global-require': 0,
   },
 };
